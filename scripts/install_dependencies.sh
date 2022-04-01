23ea95a353ffda69e757f31de442dce93d9d48fa
praful parmar <41146328+praful-parmar@users.noreply.github.com>
2022-04-01 19:41:41 +0530

    Update install_dependencies.sh

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 apt-get -y update
 
-pwd
-id
-ls -a
-pwd
-cd /home/ubuntu/app
+# pwd
+# id
+# ls -a
+# pwd
+# cd /home/ubuntu/app
 ls -a
 sudo rm -rf node_modules package-lock.json
 source /home/ubuntu/.nvm/nvm.sh
