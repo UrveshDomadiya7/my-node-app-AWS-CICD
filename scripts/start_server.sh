92b78be614e593f748c53c44bb6d4a1c08537913
bharatsimform <85876798+bharatsimform@users.noreply.github.com>
2022-04-01 20:12:05 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -6,4 +6,4 @@ source /home/ubuntu/.nvm/nvm.sh
 cd /var/www/my-node-app/
 sudo npm install pm2@latest -g
 
-/home/ubuntu/.nvm/versions/node/v16.14.2/bin/pm2 start server.js
+sudo pm2 start server.js
