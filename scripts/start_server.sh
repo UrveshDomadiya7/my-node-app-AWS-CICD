e50640e13c5c8057d5c3c120f8b90c9cabf45faa
praful parmar <41146328+praful-parmar@users.noreply.github.com>
2022-04-01 20:15:56 +0530

    Update start_server.sh

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -4,6 +4,6 @@ source /home/ubuntu/.bashrc
 source /home/ubuntu/.nvm/nvm.sh
 
 cd /var/www/my-node-app/
-sudo npm install pm2@latest -g
+# sudo npm install pm2@latest -g
 
-sudo pm2 start server.js
+/home/ubuntu/.nvm/versions/node/v16.14.2/bin/pm2 start server.js
