b508f9f3aa6b65840ec94db066410692e9d18ded
JAY HARNESHA <82324338+HARNESHA@users.noreply.github.com>
2022-04-25 10:14:12 +0530

    application is updated with server.js file

--- server.js
+++ server.js
@@ -3,9 +3,9 @@ const app = express();
 
 app.get('/healthcheck', (req, res)=>{
     if(process.env.ENV_NODE === "production")
-        res.status(200).send({"code": 2000, "msg": "Production Mode: Healthcheck is success by jay harnesha"})
+        res.status(200).send({"code": 2000, "msg": "Production Mode: Healthcheck is success"})
     else
-        res.status(200).send({"code": 2000, "msg": "Local Mode: Healthcheck is success jay harnesha"})
+        res.status(200).send({"code": 2000, "msg": "Local Mode: Healthcheck is success"})
 })
 
 app.listen(3000, () =>{
